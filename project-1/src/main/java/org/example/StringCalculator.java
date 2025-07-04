c8a80ba76834b5e3c155a0af0fe433a7fb7b2c3c
kavyan07 <kavyanp0710@gmail.com>
2025-07-04 11:51:18 +0530

    create StringCalculator class to make test pass - GREEN

--- project-1/src/main/java/org/example/StringCalculator.java
+++ project-1/src/main/java/org/example/StringCalculator.java
@@ -1,6 +1,12 @@
 package org.example;
 
-public class Main {
+public class StringCalculator {
+    public int add(String numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
