a4c56c5c32859df62dc9961ef57614852f96ec10
kavyan07 <kavyanp0710@gmail.com>
2025-07-04 11:44:23 +0530

    add first test for empty string - RED

---
+++ project-1/src/test/java/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringCalculatorTest {
+
+    @Test
+    void testEmptyStringReturnsZero() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    }
+}
