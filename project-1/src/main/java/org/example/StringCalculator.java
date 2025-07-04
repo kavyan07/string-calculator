001b562a700be1bfb9b26abae1f32da6f4abb3b8
kavyan07 <kavyanp0710@gmail.com>
2025-07-04 12:50:10 +0530

    add test for single number - RED

--- project-1/src/main/java/org/example/StringCalculator.java
+++ project-1/src/main/java/org/example/StringCalculator.java
@@ -7,6 +7,12 @@ public class StringCalculator {
         }
         return 0;
     }
+    @Test
+    void testSingleNumberReturnsThatNumber() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.add("1"));
+        assertEquals(5, calculator.add("5"));
+    }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
