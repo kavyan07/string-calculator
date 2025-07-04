20970c0f9c4c2e784a603ee070f96a8f4c30283c
kavyan07 <kavyanp0710@gmail.com>
2025-07-04 13:06:04 +0530

    implement single number parsing - GREEN"

--- project-1/src/main/java/org/example/StringCalculator.java
+++ project-1/src/main/java/org/example/StringCalculator.java
@@ -1,12 +1,14 @@
 package org.example;
 
 public class StringCalculator {
-    public int add(String numbers) {
-        if (numbers == null || numbers.isEmpty()) {
-            return 0;
+
+        public int add(String numbers) {
+            if (numbers == null || numbers.isEmpty()) {
+                return 0;
+            }
+
+            return Integer.parseInt(numbers);
         }
-        return 0;
-    }
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
